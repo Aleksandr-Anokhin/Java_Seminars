fd708c52f613f44958c6d927b17472cf12015b43
Aleksandr-Anokhin <aleksandr0788@gmail.com>
2023-01-31 18:08:41 +0300

    Added logging

--- HW_3.java
+++ HW_3.java
@@ -1,7 +1,20 @@
+import java.io.IOException;
 import java.util.Scanner;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class HW_3 {
-    public static void main(String[] args) {
+    
+    public static void main(String[] args) throws IOException {
+        Logger logger = Logger.getLogger(HW_3.class.getName());
+        ConsoleHandler ch = new ConsoleHandler();
+
+        logger.addHandler(ch);
+        SimpleFormatter sFormat = new SimpleFormatter();
+        ch.setFormatter(sFormat);
+
         double num1;
         double num2;
         double ans;
@@ -10,10 +23,10 @@ public class HW_3 {
             System.out.print("Введите через пробел два числа: ");
             num1 = reader.nextDouble();
             num2 = reader.nextDouble();
-            System.out.print("\nВведите один из операторов (+, -, *, /): ");
-            op = reader.next().charAt(0);
+            System.out.print("\nВведите один из операторов (+, -, *, /): \n");
+            op = reader.next().charAt(0);            
         }
-        switch(op) {
+        switch(op) {        
            case '+': ans = num1 + num2;
               break;
            case '-': ans = num1 - num2;
@@ -22,26 +35,15 @@ public class HW_3 {
               break;
            case '/': ans = num1 / num2;
               break;
-           default:  System.out.printf("Ошибка ввода, попробуйте ещё раз.");
-              return;
+           default: logger.log(Level.WARNING, "Input error, try again."); 
+              return;        
         }
+        
         System.out.print("\nРезультат:\n");
         System.out.printf(num1 + " " + op + " " + num2 + " = " + ans);
-        System.out.print(' ');  // Пустая срока как разделитель
-        System.out.print("\nПовторить - 1; Завершить - 0.\n");  // Пользователю предлагается выбыр, что делать дальше.
-        // Непонятно, как связать этот выбор с функцией.
+        
+        logger.info("Test log");
+        
     }
-    /* 
-    public static int choosingAnAction(int e) {
-        Scanner iScanner = new Scanner(System.in);
-        System.out.printf("Выберите действие: ");
-        int e = iScanner.nextInt();
-            if (e == 0) {
-                System.exit(0);
-            }
-            if (e == 1) {
-                // Здесь по должен быть вызов программы с начала, но непонятно что именно сюда записать
-            }
-    }
-    */    
+    
 }
